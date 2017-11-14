d25e8e74235de6de34a8aac046db9fe0df814d72
StopllL <mganxiaolei@gmail.com>
2017-11-14 18:22:51 +0800

    blog project init

---
+++ src/components/Nav/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './index.css'
+
+class Nav extends Component {
+    render() {
+        return (
+            <nav className="nav">
+                <p>请登录</p>
+            </nav>
+        )
+    }
+}
+
+export default Nav
